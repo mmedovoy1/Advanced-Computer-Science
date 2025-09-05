74cf70ea69ece207601999541bede0d670211b83
mmedovoy1 <mmedovoy1@hwemail.com>
2025-09-05 08:07:21 -0700

    U0D3

--- Unit Lessons/U0D2/Zombie.java
+++ Unit Lessons/U0D2/Zombie.java
@@ -18,6 +18,13 @@ public class Zombie {
         Zombie brian = new Zombie();
 
         System.out.println(brian.growl());
+
+        System.out.println(brian.humansEaten());
+        brian.eatHuman();
+        System.out.println(brian.humansEaten());
+        brian.feast(14);
+        System.out.println(brian.humansEaten());
+
     }
     
     // methods (behaviors)
@@ -27,8 +34,17 @@ public class Zombie {
         return "grrrrrrr";
     }
 
+    // Challenge: make a new method that returns the number of humans eaten
     public int humansEaten() {
         return humansEaten;
     }
     
+    public void eatHuman() {
+        humansEaten++;
+    }
+
+    public void feast(int humanCount){
+        humansEaten += humanCount;
+    }
+
 }
---
+++ Unit Lessons/U0D3/Lesson.java
@@ -0,0 +1,33 @@
+public class Lesson {
+
+    public static void main(String[] args) {
+        // variables
+        int number = 5; // assignment operator
+
+        // single = assigns a value
+        // different from ==, which compares a value
+
+        // different assignment operators
+        number += 6; // same as number = number + 6
+        number -= 3; // number = number - 3
+        number *= 10; // number = number * 10
+        number /= 2; // number = number / 2
+
+        // incrememnt and decrement operators
+        number += 1;
+        number++;
+
+        number -= 1; 
+        number--;
+
+        // boolean operators, evaluate to true or false
+        // use them in if statements
+
+        // You cannot use this for objects
+        
+        //number == 5; // equals
+        //number != 10 // not equals
+        //number <= 15 // less than or equals
+        //number >= 20 // greater than or equals
+    }
+}
