124c290ed4147b30309481251f05ae73272c0706
mmedovoy1 <mmedovoy1@hwemail.com>
2025-11-11 13:31:05 -0800

    U2D3

---
+++ Assignments/Unit 2/Lab 2.2 - ArrayOps/ArrayOps.java
@@ -0,0 +1,155 @@
+public class ArrayOps {
+
+    /**
+     * Prints the contents of a string array, separating each element with a
+     * comma and enclosing the output in square brackets [].
+     *
+     * @param array
+     *              The String array to be printed.
+     * @return The constructed string representation of the array.
+     */
+    public static String printStringArray(String[] array) {
+        String toReturn = "";
+        for (int i = 0; i < array.length; i++) {
+            if (i == 0) {
+                toReturn += array[0];
+            } else {
+                toReturn = toReturn + ", " + array[i];
+            }
+        }
+        String toPrint = "[" + toReturn + "]";
+        System.out.println(toPrint);
+        return toPrint;
+    }
+
+    /**
+     * Prints the contents of an int array, separating each element with a comma and
+     * enclosing the output in square brackets [].
+     *
+     * @param array
+     *              The int array to be printed.
+     * @return The String representation of the array.
+     */
+
+    public static String printIntegerArray(int[] array) {
+                String toReturn = "";
+        for (int i = 0; i < array.length; i++) {
+            if (i == 0) {
+                toReturn += array[0];
+            } else {
+                toReturn = toReturn + ", " + array[i];
+            }
+        }
+        String toPrint = "[" + toReturn + "]";
+        System.out.println(toPrint);
+        return toPrint;
+    }
+
+    /**
+     * The method finds the largest integer in the array.
+     *
+     * @param array
+     *              The int array to find the maximum value from.
+     * @return The largest integer in the array.
+     */
+    public static int findMax(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            
+        }
+    }
+
+    /**
+     * The method returns the longest String in the array.
+     * If two Strings share the longest length, the method will return the one that
+     * appears first in the array.
+     *
+     * @param array
+     *              The String array to find the longest String from.
+     * @return The longest String in the array.
+     */
+    public static String findLongestString(String[] array) {
+        return "";
+    }
+
+    /**
+     * Calculates the average length of all the Strings in the array.
+     *
+     * @param array
+     *              The String array to find the average string length from.
+     * @return The average length of all the Strings in the array.
+     */
+    public static double averageStringLength(String[] array) {
+        return 0.0;
+    }
+
+    /**
+     * Counts the number of times each letter appears in the given String.
+     * The method creates an array of integers length 26, where each element
+     * represents the
+     * frequency of a letter in the alphabet. The first element (index 0) represents
+     * the frequency of the letter 'a', the second element (index 1) represents the
+     * frequency of the letter 'b', and so on.
+     *
+     * @param input
+     *              The input string to count letter frequencies from.
+     * @return An int array representing the frequency of each letter in the input
+     *         string.
+     */
+    public static int[] countLetterFrequencies(String input) {
+        return new int[26];
+    }
+
+    /**
+     * Removes the indicated element, shifts the index of all the elements down by
+     * one, and assigns a value of zero to the last element in the array.
+     *
+     * @param array
+     *              The array of int values
+     * @param index
+     *              The index of the element to be removed
+     * @return The updated array
+     */
+    public static int[] removeIntAndScoot(int[] array, int index) {
+        return new int[0];
+    }
+
+    /**
+     * Resizes the input array to twice its size.
+     *
+     * @param array
+     *              The input array of integers
+     * @return The resized array
+     */
+    public static int[] resizeIntArray(int[] array) {
+        return new int[0];
+    }
+
+    /**
+     * Adds the number symbol (#) and the element number (index) to the beginning of
+     * each
+     * String in the array.
+     * For example, if the array contained: {"Hello", "my", "name", "is", "Larry"},
+     * then this method would return an array with the contents:
+     * {"#0 Hello", "#1 my", "#2 name", "#3 is", "#4 Larry"}.
+     *
+     * @param array
+     *              The array of Strings to modify
+     * @return The modified array with the number symbol and element number added to
+     *         each String
+     */
+    public static String[] addNumToStringArray(String[] array) {
+        return new String[0];
+    }
+
+    /**
+     * Reverses the order of the elements in the given integer array.
+     *
+     * @param array
+     *              The array to be reversed
+     * @return The reversed array
+     */
+    public static int[] reverseIntArray(int[] array) {
+        return new int[0];
+    }
+
+}
