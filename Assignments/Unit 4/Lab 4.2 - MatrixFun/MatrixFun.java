aa27ea1fcef6db059560fcdb2f9e8ccf24d3136b
mmedovoy1 <mmedovoy1@hwemail.com>
2026-02-02 13:27:34 -0800

    worked on matrixFun

--- Assignments/Unit 4/Lab 4.2 - MatrixFun/MatrixFun.java
+++ Assignments/Unit 4/Lab 4.2 - MatrixFun/MatrixFun.java
@@ -1,11 +1,12 @@
 public class MatrixFun {
     private int numberOfRows;
     private int numberOfCols;
-    private int[][] matrix = new int[numberOfRows][numberOfCols];
+    private int[][] matrix;
 
     public MatrixFun(int numberOfRows, int numberOfCols) {
        this.numberOfRows = numberOfRows;
        this.numberOfCols = numberOfCols;
+       this.matrix = new int[numberOfRows][numberOfCols];
     }
 
     public MatrixFun(int[][] starterMatrix) {
@@ -43,7 +44,7 @@ public class MatrixFun {
 
     public String toString(){
         String toReturn = "";
-        int signs = this.getNumberOfCols() * 2 - 1;
+        int signs = ((this.getNumberOfCols() * 2) - 1);
         for (int i = 0; i < signs; i++) {
             toReturn += "=";
         }
@@ -61,6 +62,54 @@ public class MatrixFun {
     }
 
     public boolean equals(MatrixFun other) {
+        if (matrix[0].length != other.getMatrix()[0].length && matrix.length != other.getMatrix().length) {
+            return false;
+        }
+        boolean doesEqual = true;
+        int count = -1;
+        for (int[] row : matrix) {
+            count += 1;
+            for (int cell : row) {
+                if (matrix[count][cell] != other.getMatrix()[count][cell]) {
+                    return false;
+                }
+            }
+        }
+        return doesEqual;
+    }
 
+    public boolean equals(int[][] other) {
+        if (matrix[0].length != other[0].length && matrix.length != other.length) {
+            return false;
+        }
+        boolean doesEqual = true;
+        int count = -1;
+        for (int[] row : matrix) {
+            count += 1;
+            for (int cell : row) {
+                if (matrix[count][cell] != other[count][cell]) {
+                    return false;
+                }
+            }
+        }
+        return doesEqual;
+    }
+
+    public void replaceAll(int oldValue, int newValue) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == oldValue) {
+                    matrix[i][j] = newValue;
+                }
+            }
+        }
+    }
+
+    public void swapRow(int rowA, int rowB) {
+        int[][] matrix2 = matrix;
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][rowA] = matrix[i][rowB];
+            matrix[i][rowB] = matrix2[i][rowB];
+        }
     }
 }
---
+++ Assignments/Unit 4/Lab 4.2 - MatrixFun/MatrixFunTester.java
@@ -0,0 +1,8 @@
+public class MatrixFunTester {
+    public static void main(String[] args) {
+        int[][] table  = {{1,2,3},{1,2,3}};
+        MatrixFun matrix = new MatrixFun(table);
+        System.out.println(matrix.toString());
+    }
+
+}
